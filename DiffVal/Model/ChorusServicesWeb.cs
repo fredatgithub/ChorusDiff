1c96812fe2bfc4b71501dff4381c899884421858
Fred <domaine75@gmail.com>
2020-05-29 20:35:42 +0200

    Initial commit

---
+++ DiffVal/Model/ChorusServicesWeb.cs
@@ -0,0 +1,60 @@
+﻿namespace DiffVal.Model
+{
+    public class ChorusServicesWeb
+    {
+        //	Identifiant	RaisonSociale	Code	Nom	GestionEGMT	ServiceActif	Adresse	ComplementAdresse1	ComplementAdresse2	CodePostal	Ville	NumTelephone	Courriel	CodePays	LibellePays
+        public string Identifiant { get; set; }
+        public string RaisonSociale { get; set; }
+        public string Code { get; set; }
+        public string Nom { get; set; }
+        public bool GestionEGMT { get; set; }
+        public bool ServiceActif { get; set; }
+        public string Adresse { get; set; }
+        public string ComplementAdresse1 { get; set; }
+        public string ComplementAdresse2 { get; set; }
+        public string CodePostal { get; set; }
+        public string Ville { get; set; }
+        public string NumTelephone { get; set; }
+        public string Courriel { get; set; }
+        public string CodePays { get; set; }
+        public string LibellePays { get; set; }
+
+        public ChorusServicesWeb(string Identifiant,
+         string RaisonSociale,
+         string Code,
+         string Nom,
+         bool GestionEGMT,
+         bool ServiceActif,
+         string Adresse,
+         string ComplementAdresse1,
+         string ComplementAdresse2,
+         string CodePostal,
+         string Ville,
+         string NumTelephone,
+         string Courriel,
+         string CodePays,
+         string LibellePays)
+        {
+            this.Identifiant = Identifiant;
+            this.RaisonSociale = RaisonSociale;
+            this.Code = Code;
+            this.Nom = Nom;
+            this.GestionEGMT = GestionEGMT;
+            this.ServiceActif = ServiceActif;
+            this.Adresse = Adresse;
+            this.ComplementAdresse1 = ComplementAdresse1;
+            this.ComplementAdresse2 = ComplementAdresse2;
+            this.CodePostal = CodePostal;
+            this.Ville = Ville;
+            this.NumTelephone = NumTelephone;
+            this.Courriel = Courriel;
+            this.CodePays = CodePays;
+            this.LibellePays = LibellePays;
+        }
+
+        public string ToWebCSVString(string separator)
+        {
+            return $"{Identifiant}{separator}{RaisonSociale}{separator}{Code}{separator}{Nom}{separator}{GestionEGMT}{separator}{ServiceActif}{separator}{Adresse}{separator}{ComplementAdresse1}{separator}{ComplementAdresse2}{separator}{CodePostal}{separator}{Ville}{separator}{NumTelephone}{separator}{Courriel}{separator}{CodePays}{separator}{LibellePays}";
+        }
+    }
+}
---
+++ DiffVal/Program.cs
@@ -0,0 +1,134 @@
+﻿using DiffVal.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+
+namespace DiffVal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> display = Console.WriteLine;
+            string file1 = "AIFE-Chorus-Pro-Annuaire_06042020.csv";
+            string file2 = "AIFE-Chorus-Pro-Annuaire_20200525-services-only.csv";
+            char separator = ';';
+            var listeTable = new List<ChorusServicesTable>();
+            var listeWeb = new List<ChorusServicesWeb>();
+            bool hasHeader = true;
+            int indexReader = 0;
+            using (StreamReader streamReader = new StreamReader(file1))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    if (indexReader == 0 && hasHeader)
+                    {
+                        string line2 = streamReader.ReadLine();
+                        indexReader++;
+                        continue;
+                    }
+
+                    string line = streamReader.ReadLine();
+                    indexReader++;
+                    var lineArray = line.Split(separator);
+                    bool gestionEGMT = lineArray[5].ToLower() == "true" ? true : false;
+                    bool serviceActif = lineArray[6].ToLower() == "true" ? true : false;
+
+                    var oneChorusService = new ChorusServicesTable(
+                        lineArray[0],
+                        lineArray[1],
+                        lineArray[2],
+                        lineArray[3],
+                        lineArray[4],
+                        gestionEGMT,
+                        serviceActif,
+                        lineArray[7],
+                        lineArray[8],
+                        lineArray[9],
+                        lineArray[10],
+                        lineArray[11],
+                        lineArray[12],
+                        lineArray[13],
+                        lineArray[14],
+                        lineArray[15]
+                        );
+
+                    listeTable.Add(oneChorusService);
+                }
+            }
+
+            indexReader = 0;
+            hasHeader = true;
+            using (StreamReader streamReader2 = new StreamReader(file2))
+            {
+                while (!streamReader2.EndOfStream)
+                {
+                    if (indexReader == 0 && hasHeader)
+                    {
+                        string line2 = streamReader2.ReadLine();
+                        indexReader++;
+                        continue;
+                    }
+
+                    string line = streamReader2.ReadLine();
+                    indexReader++;
+                    var lineArray = line.Split(separator);
+                    bool gestionEGMT = lineArray[4].ToLower() == "true" ? true : false;
+                    bool serviceActif = lineArray[5].ToLower() == "true" ? true : false;
+
+                    var oneChorusService = new ChorusServicesWeb(
+                        lineArray[0],
+                        lineArray[1],
+                        lineArray[2],
+                        lineArray[3],
+                        gestionEGMT,
+                        serviceActif,
+                        lineArray[6],
+                        lineArray[7],
+                        lineArray[8],
+                        lineArray[9],
+                        lineArray[10],
+                        lineArray[11],
+                        lineArray[12],
+                        lineArray[13],
+                        lineArray[14]
+                        );
+
+                    listeWeb.Add(oneChorusService);
+                }
+            }
+
+            var listeDiff = new List<ChorusServicesWeb>();
+            var diffIdentifiant = listeWeb.Where(p => !listeTable.Any(p2 => p2.Identifiant == p.Identifiant));
+            var diffIdentifiantAndCode = listeWeb.Where(p => !listeTable.Any(p2 => p2.Identifiant == p.Identifiant && p2.Code == p.Code));
+            var lesIdentifiants = diffIdentifiant.ToList();
+            var lesIdentifiantsEtLesCodes = diffIdentifiantAndCode.ToList();
+            display($"diff identifiants : {lesIdentifiants.Count}"); // 553
+            display($"diff identifiants et les codes : {lesIdentifiantsEtLesCodes.Count}"); // 848
+
+            using (StreamWriter sw = new StreamWriter("identifiants.csv"))
+            {
+                foreach (var item in lesIdentifiants)
+                {
+                    string test = item.ToWebCSVString(separator.ToString());
+                    sw.WriteLine(item.ToWebCSVString(separator.ToString()));
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter("lesIdentifiantsEtLesCodes.csv"))
+            {
+                foreach (var item in lesIdentifiantsEtLesCodes)
+                {
+                    string test = item.ToWebCSVString(separator.ToString());
+                    sw.WriteLine(item.ToWebCSVString(separator.ToString()));
+                }
+            }
+
+            display("Press any key to exit:");
+            Console.ReadKey();
+        }
+
+    }
+}
